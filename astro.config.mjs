5f6dfca21ab5403e6b3d6c24d9784029cfa9a1d8
Keyur Patel <ekeyur@gmail.com>
2022-09-06 22:11:51 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap(), image()],
+});
